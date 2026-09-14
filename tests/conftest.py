b082500eb29f35dcfd55b3bf67317fdd499b46bf
AlexandroSanna <122700787+AlexandroSanna@users.noreply.github.com>
2026-09-14 17:22:51 +0200

    Formattati test

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,9 +63,10 @@ def tank_info_df_grape_variety_split():
 def monkey_joblib(monkeypatch):
     def execute_parallel(*args, **kwargs):
         return [
-        func(*inner_args, **call_kwargs)
-        for func, inner_args, call_kwargs in args[0]
-    ]
+            func(*inner_args, **call_kwargs)
+            for func, inner_args, call_kwargs in args[0]
+        ]
+
     fake_parallel_mock = Mock(side_effect=execute_parallel)
     fake_delayed_mock = Mock(wraps=joblib.delayed)
 
